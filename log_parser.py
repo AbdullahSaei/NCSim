c0e7653eccbec2def40cbca4aaf0bb2557da7b6f
abdullah-server <abdullahsaei@hotmail.com>
2021-08-12 18:17:19 +0200

    log parser progress

--- log_parser.py
+++ log_parser.py
@@ -6,13 +6,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-# import re
 
 LOG_PATH = "logs/"
-LOG_FILE = "random_14_Simple_WSN_3"
+LOG_FILE = "random_20_Simple_WSN_3"
 
 
-def preprocess_at_tx():
+def prepare_at_tx():
     """
     Prepares df_at_tx dataframe
 
@@ -35,31 +34,71 @@ def preprocess_at_tx():
     df_aods = df_at_tx_aods.melt('Generation',
                                  var_name='Algorithm', value_name='Availability of Data percentage')
 
-    return df_aods, df_ranks
+    return df_aods, df_ranks, list(map(int, [rnd_num, nodes_num, gen_num]))
 
 
-def plots_at_tx():
-    pass
-
-
-def main():
-    pass
-
-
-def preprocessing(data_frame) -> pd.DataFrame:
+def prepare_at_done():
     """
-    Prepare data for analysis
+    Prepares df_at_done dataframe
+
+    RETURN
+    ------
+    df_aods, df_ranks
     """
-    # wide to long df_at_tx
-    # df_ready = pd.wide_to_long(data_frame)
-    df_ready = data_frame
-    return df_ready
+
+    gens = {g for g in df_at_done['Generation']}
+    algs = {alg for alg in df_at_done['Algorithm']}
+    at_done_means = []
+    at_done_maxes = []
+    for g in gens:
+        for alg in algs:
+            at_done_means.append(df_at_done.query(
+                f"Generation == {g} and Algorithm == '{alg}'").mean())
+            at_done_maxes.append(df_at_done.query(
+                f"Generation == {g} and Algorithm == '{alg}'").max())
+
+    df_at_done_maxes = pd.concat(at_done_maxes, axis=1).T
+    df_at_done_means = pd.concat(at_done_means, axis=1).T
+    df_at_done_means['Algorithm'] = df_at_done_maxes['Algorithm']
+
+    return df_at_done_means, df_at_done_maxes
+
+
+def plots_at_tx(rnd_num, nodes_num):
+    plt.figure()
+    sns.boxplot(data=df_aods, x="Availability of Data percentage",
+                y="Algorithm").set_title(f"AoD at tx = {rnd_num} of {nodes_num} nodes")
+    plt.figure()
+    sns.boxplot(data=df_ranks, y="Ranks",
+                x="Algorithm").set_title(f"Ranks at tx = {rnd_num} of {nodes_num} nodes")
+
+
+def plots_at_done(rnd_num, nodes_num):
+    plt.figure()
+    sns.boxplot(data=df_done_maxes, x="Round", y="Algorithm").set_title(
+        f"Max num of rounds when done {nodes_num} nodes")
+    add_v_line(rnd_num)
+    plt.figure()
+    sns.boxplot(data=df_done_means, x="Round", y="Algorithm").set_title(
+        f"Mean num of rounds when done {nodes_num} nodes")
+    add_v_line(rnd_num)
+
+
+def add_v_line(pos):
+    plt.axvline(pos, ls='--', color="red",)
+    plt.text(pos + 0.1, 0.1, f"tx = {pos}", color='tab:red',
+                rotation=270)
+
+
+def main(rnd_num, nodes_num, gen_num):
+    plots_at_tx(rnd_num, nodes_num)
+    plots_at_done(rnd_num, nodes_num)
 
 
 if __name__ == '__main__':
 
     # load the file
-    df_at_tx = preprocessing(pd.read_csv(LOG_PATH + LOG_FILE + "_at_tx.csv"))
+    df_at_tx = pd.read_csv(LOG_PATH + LOG_FILE + "_at_tx.csv")
     df_at_done = pd.read_csv(LOG_PATH + LOG_FILE + "_at_done.csv")
 
     # visualize data
@@ -73,6 +112,7 @@ if __name__ == '__main__':
     print(df_at_done.head())
     print(df_at_done.sample(3))
 
-    df_aods, df_ranks = preprocess_at_tx()
+    df_aods, df_ranks, configs = prepare_at_tx()
+    df_done_means, df_done_maxes = prepare_at_done()
 
-    main()
+    main(*configs)
